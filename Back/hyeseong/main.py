738f29f953beb0fa8bf02ba16362233258169cdd
hyesung <khs990419@gmail.com>
2019-02-22 00:20:01 +0900

    add method list

--- Back/hyeseong/main.py
+++ Back/hyeseong/main.py
@@ -3,24 +3,28 @@ import request
 
 app = Flask(__name__)
 
-'''@app.route('/')
-def main_page():
-    return render_template('메인페이지.html')'''
-
 @app.route('/')
+def main_page():
+    return render_template('boardList.html')
+
+'''@app.route('/')
 def main_get(f=None):
-    return render_template('boardWriteForm.html', f=f)
+    return render_template('boardWriteForm.html', f=f)'''
 
 '''@app.route('/')
 def css_file():
     return redirect(url_for('static', filename='CSS.css'))'''
 
-@app.route('/register', methods=['POST']) #boardWriteForm.html
+@app.route('/boardWriteForm.html', methods=['POST']) #boardWriteForm.html
 def register(f=None):
     if request.method == 'POST':
         temp1 = request.form['f'] #f가 json?
         temp2 = json.loads(x) #json->python dictionary
-        return temp2 #혹은 render_template
+        return render_template('boardWriteForm.html')
+
+@app.route('/boardList.html')
+def list():
+    return render_template('boardList.html')
 
 @app.route('/pass_data_to_db')
 
