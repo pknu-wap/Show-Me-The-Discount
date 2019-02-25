ab187b4f9d82331cde0acb75bf9c26dd08154fcf
hyesung <khs990419@gmail.com>
2019-02-25 22:32:20 +0900

    add method register

--- Back/hyeseong/main.py
+++ Back/hyeseong/main.py
@@ -7,21 +7,21 @@ def main_page():
     #db
     return render_template('boardList.html')
 
-'''@app.route('/')
-def css_file():
-    return redirect(url_for('static', filename='CSS.css'))'''
-
 @app.route('/boardWriteForm.html')
 def write():
     return render_template('boardWriteForm.html')
 
-@app.route('/boardProcess.html', methods = ['POST'])
-def process():
-    if request.method == "POST":
-        result = request.form
-        #db
-
-        return #등록 알림창, 페이지 이동
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        store = request.form['store']
+        product = request.form['product']
+        content = request.form['content']
+        price = request.form['price']
+        startdate = request.form['startdate']
+        enddate = request.form['enddate']
+        password = request.form['password']
+        return redirect(url_for('view', ))
 
 @app.route('/boardList.html')
 def list():
@@ -29,11 +29,13 @@ def list():
 
 @app.route('/boardView.html')
 def view():
-    post_id = request.args.get("글번호")
+    post_id = request.args.get('글번호')
     #db
     return render_template('boardView.html')
 
 @app.route('/boardModifyForm.html')
+def modify():
+    return render_template('boardModifyForm.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
