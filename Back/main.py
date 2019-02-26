11b6a0c331d3e49c33709c2b4d358dfee8005bec
hyesung <khs990419@gmail.com>
2019-02-26 20:57:13 +0900

    add a variable in path

--- Back/main.py
+++ Back/main.py
@@ -4,36 +4,52 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    #db
     return render_template('boardList.html')
 
 @app.route('/boardWriteForm.html')
 def write_form():
     return render_template('boardWriteForm.html')
 
-@app.route('/json_data.txt', methods=['POST', 'GET'])
+@app.route('/boardView.html/<id>', methods=['POST'])
+def register(id):
+    if request.method == 'POST':
+        id = request.form["id"]
+        store = request.form["store"]
+        product = request.form["product"]
+        content = request.form["content"]
+        price = request.form["price"]
+        startdate = request.form["start"]
+        enddate = request.form["end"]
+        password = request.form["password"]
+        #db
+        return redirect('/boardView.html/'+id)
+
+'''@app.route('/json_data.txt', methods=['POST', 'GET'])
 def json():
     if request.method == 'POST':
         print(request.headers.get('product'))
-    #db
+        #db
         return jsonify(request.headers.get('product'))
     elif request.method == 'GET':
-        return jsonify(request.headers.get('product'))
+        return jsonify(request.headers.get('product'))'''
 
 @app.route('/boardList.html', methods=['GET'])
 def list():
-
+    #db
     return render_template('boardList.html')
 
-@app.route('/boardView.html')
-def view1():
-    #post_id = request.args.get('글번호')
-    return render_template('boardView.html')
+@app.route('/boardView.html/<id>')
+def view1(id):
+    post_id = request.args.get('id')
+    #db
+    return redirect(url_for('view2', id=id))
 
 @app.route('/boardView.html/')
-def view2():
-    dict = {id : '1', product : '상품'}
-    return jsonify(dict)
+def view2(id):
+    '''dict = {id : '1', product : '상품'}
+    return jsonify(dict)'''
+    #db
+    return render_template('boardView', )
 
 @app.route('/boardModifyForm.html')
 def modify():
