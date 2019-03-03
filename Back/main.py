f55f49bf33f4857e5ba89e9dfffe9dc687a0697e
hyesung <khs990419@gmail.com>
2019-03-03 15:44:27 +0900

    make delete method

--- Back/main.py
+++ Back/main.py
@@ -5,14 +5,18 @@ app = Flask(__name__)
 id = 0
 
 @app.route('/')
-def main_page():
-    return render_template('boardList.html')
+@app.route('/boardList.html')
+def list():
+
+    #db 데이터 읽기
+
+    return render_template('boardList.html') #json
 
 @app.route('/boardWriteForm.html')
 def write_form():
     return render_template('boardWriteForm.html')
 
-@app.route('/boardView.html', methods=['POST'])
+@app.route('/write', methods=['POST'])
 def register():
     if request.method == 'POST':
         global id
@@ -25,25 +29,25 @@ def register():
         enddate = request.form["end"]
         password = request.form["password"]
 
-    x = {"id": id, "store": store, "product": product, "content": content, "price": price, "startdate": startdate, "enddate": enddate, "password": password}
-    y = json.dumps(x)
+        x = {"id": id, "store": store, "product": product, "content": content, "price": price, "startdate": startdate, "enddate": enddate, "password": password}
+        y = json.dumps(x)
+
     #db 데이터 삽입
+
     return redirect('/boardView.html/'+str(id))
 
 @app.route('/boardView.html/<int:id>')
 def view(id):
+
     #db 데이터 읽기
+
     return render_template('boardView.html') #json
 
-#@app.route('/')
-@app.route('/boardList.html')
-def list():
-    #db
-    x = '{"id": 1, "store": STORE, "product": PRODUCT, "content": CONTENT, "price": PRICE, "startdate": STARTDATE, "enddate": ENDDATE}' #TEST
-    return x
-    #return render_template('boardList.html')
-
 @app.route('/boardModifyForm.html')
+def modify_form():
+    return render_template('boardModifyForm.html')
+
+@app.route('/modify')
 def modify():
     if request.method == 'POST':
         id = request.form["id"]
@@ -57,8 +61,19 @@ def modify():
 
         x = {"id": id, "store": store, "product": product, "content": content, "price": price, "startdate": startdate, "enddate": enddate, "password": password}
         y = json.dumps(x)
-        #db 데이터 수정
-    return render_template('boardModifyForm.html')
+
+    #db 데이터 수정
+
+    return redirect('/boardView.html/'+str(id))
+
+@app.route('/delete1')
+def delete():
+    #db 데이터 읽기
+    #비밀번호 일치 여부 확인
+
+        #db 데이터 삭제
+
+    return render_template('boardList.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
