c9fe6b8ac108e49cd5a91511bd85924b9cc0a40c
woorst1 <woorst1@naver.com>
2019-03-10 10:18:01 +0900

    db_func_comple

--- db_insert.py
+++ db_insert.py
@@ -2,33 +2,38 @@ import pymysql
 
 db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123', db='wap', charset='utf8')
 
+print("INSERT : 입력, SELECT : 검색, UPDATE : 변경, DELETE : 삭제 ")
+value = input("옵션을 선택하시오: ")
+
 def func(value):
     if value == 'INSERT':
         try:
             with db.cursor() as cursor:
                 sql = 'INSERT INTO post (data_id, store, product, content, price, startdate, enddate, password)' \
                       ' VALUES (%d, %s, %s, %s, %d, %s, %s, %d)'
-                cursor.execute(sql, ('wap', '123'))
+                cursor.execute(sql, data)
                 db.commit()
 
             with db.cursor() as cursor:
                 sql = 'INSERT INTO replies (reply_id, num, reply) VALUES (%d, %d, %s)'
-                cursor.execute(sql, ('wap', '123'))
+                cursor.execute(sql, )
                 db.commit()
                 print(cursor.lastrowid)
 
     elif value == 'SELECT':
         with db.cursor() as cursor:
-            sql = 'SELECT store, products, content, startdate, enddate FROM post WHERE id = 1234'
+            sql = 'SELECT store, products, content, startdate, enddate FROM post WHERE data_id = 1234'
             cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result)
+            datas = cursor.fetchall()
+                for data in datas:
+                    print(data)
 
         with db.cursor() as cursor:
             sql = 'SELECT reply_id, replyContent, replyTime FROM replies WHERE data_id = 1234'
             cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result)
+            datas = cursor.fetchall()
+                for data in datas:
+                    print(data)
 
     elif value == 'update':
         with db.cursor() as cursor:
@@ -46,7 +51,6 @@ def func(value):
 
         finally:
             db.close()
-
 
 
 
