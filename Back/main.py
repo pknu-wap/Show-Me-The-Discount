ebbee6b780b6ceb67d1213ce41af970d2688962b
woorst1 <woorst1@naver.com>
2019-03-11 20:26:12 +0900

    .

--- Back/main.py
+++ Back/main.py
@@ -16,27 +16,27 @@ reply_id = 0
 @app.route('/boardList.html')
 def list():
     with db.cursor() as cursor:
-            sql = '''
-                CREATE TABLE post(
-                    PRIMARY KEY(Data_Id) NOT NULL,
-                    Store VARCHAR(10) NOT NULL,
-                    Product VARCHAR(10) NOT NULL,
-                    Content VARCHAR(20) NOT NULL,
-                    Price INT NOT NULL,
-                    Start_Date NULL,
-                    End_Date VARCHAR(10) NOT NULL,
-                    Password INT NOT NULL
+        sql = '''
+            CREATE TABLE post(
+                PRIMARY KEY(Data_Id) NOT NULL,
+                Store VARCHAR(10) NOT NULL,
+                Product VARCHAR(10) NOT NULL,
+                Content VARCHAR(20) NOT NULL,
+                Price INT NOT NULL,
+                Start_Date NULL,
+                End_Date VARCHAR(10) NOT NULL,
+                Password INT NOT NULL
                 );
 
-                CREATE TABLE replies(
-                    PRIMARY KEY(Reply_Id) NOT NULL,
-                    Reply VARCHAR(20) NOT NULL.
-                    Num INT NOT NULL
+            CREATE TABLE replies(
+                PRIMARY KEY(Reply_Id) NOT NULL,
+                Reply VARCHAR(20) NOT NULL.
+                Num INT NOT NULL
                 );
-                  '''
+                '''
 
-        cursor.execute(sql)  # 실행하기
-    db.commit()  # DB에 Complete
+    cursor.execute(sql)  # 실행하기
+db.commit()  # DB에 Complete
     #finally:
         #db.close()  # DB 연결 닫기
     #db show 함수
